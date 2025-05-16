56646f988afccad281931a658c92c5a1c4ef24ed
Yuka Saito <youxiazhaiteng67@gmail.com>
2025-05-16 11:50:00 +0900

    Update XGBoost feature importance.py

--- feature importance/XGBoost feature importance.py
+++ feature importance/XGBoost feature importance.py
@@ -1,8 +1,9 @@
+model.fit(x_train, y_train, eval_set=[(x_val, y_val)], verbose=100)
 booster = model.get_booster()
 """
-.booster_：LightGBM の 内部の Booster クラス（純粋な学習器）へのアクセス。
+.get_booster()：LightGBM の 内部の Booster クラス（純粋な学習器）へのアクセス。
 """
-fscore = model.get_score(importance_type="total_gain")
+fscore = booster.get_score(importance_type="total_gain")
 fscore = sorted([(k, v) for k, v in fscore.items()], key=lambda tpl: tpl[1], reverse=True)
 print(f"\n {name} importance")
 print(fscore)
